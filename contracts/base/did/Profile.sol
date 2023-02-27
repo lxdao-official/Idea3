c54edd7cf35622df3f541f15e3db1c2bc18f13ff
0x1998 <0x199801@gmail.com>
2023-02-27 23:01:08 +0800

    Add Profile contract

---
+++ contracts/base/did/Profile.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Interface for the Profile contract
+/// @author 1998
+/// @notice This interface is used to set and get the profile of an address
+contract Profile {
+    // ...
+}
